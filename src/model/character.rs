8bec932c9827259366ec786b4928d924e08c2dc2
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2023-05-20 17:32:53 +0200

    Adds Character builder

--- src/model/character.rs
+++ src/model/character.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
 /// Represents a Character
-#[derive(Debug, Deserialize, Serialize, FromRow)]
+#[derive(Debug, Default, Deserialize, Serialize, FromRow, Clone)]
 pub struct Character {
     id: Option<i64>,
     name: String,
@@ -29,5 +29,50 @@ impl Character {
     /// Returns a reference to Character's image url
     pub fn image_url(&self) -> &String {
         &self.image_url
+    }
+}
+
+pub struct CharacterBuilder {
+    character: Character,
+}
+
+impl CharacterBuilder {
+    /// Creates an instance of CharacterBuilder
+    pub fn new() -> Self {
+        Self {
+            character: Character::default(),
+        }
+    }
+
+    /// Build the Character instance and returns it
+    pub fn build(&self) -> Character {
+        self.character.clone()
+    }
+
+    /// Sets the id value of the character instance
+    ///
+    /// # Argument
+    /// * id - the id value to set
+    pub fn id(&mut self, id: i64) -> &mut Self {
+        self.character.id = Some(id);
+        self
+    }
+
+    /// Sets the name value of the character instance
+    ///
+    /// # Argument
+    /// * name - the name value to set
+    pub fn name(&mut self, name: String) -> &mut Self {
+        self.character.name = name;
+        self
+    }
+
+    /// Sets the image url value of the character instance
+    ///
+    /// # Argument
+    /// * image_url - the image url value to set
+    pub fn image_url(&mut self, image_url: String) -> &mut Self {
+        self.character.image_url = image_url;
+        self
     }
 }
